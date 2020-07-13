4b72202001f3e73266b09aa62e3dc113a9abad9d
kyoolbaba <kyoolbaba@gmail.com>
2020-07-13 15:00:06 +0530

    Added search and Ask  input fields

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const hoverOnNAv=document.querySelector('.v');
-hoverOnNAv.addEventListener('mouseover', function(){
-hoverOnNAv.style.backgroundColor='orange';
-});
+// const hoverOnNAv=document.querySelector('.v');
+// hoverOnNAv.addEventListener('mouseenter', function(){
+// hoverOnNAv.style.backgroundColor='orange';
+// });
 
 
 const list=[
@@ -18,7 +18,7 @@ const list=[
     {
         name:'Joshea Fox',
         email:'JoshyaFox@company.com',
-        Status:'Active',
+        Status:'Inactive',
         Role:'Admin',
         LastLogin:"3 Days ago",
         Permission:'Valid'
@@ -46,7 +46,7 @@ const list=[
     {
         name:'Nicole  Larson',
         email:'Nicolelarsonn@company.com',
-        Status:'Inactive',
+        Status:'Active',
         Role:'User',
         LastLogin:'1 Days ago',
         Permission:'valid'
@@ -59,13 +59,22 @@ const list=[
         Role:'User',
         LastLogin:'2 Days ago',
         Permission:'valid'
+    },
+
+    {
+        name:'Roger Carpenter',
+        email:'RogerCarpenter@company.com',
+        Status:'Active',
+        Role:'User',
+        LastLogin:'2 Days ago',
+        Permission:'valid'
     }
 ]
 
 
 
 
-let Tableheader=['Name','Email','Status','Role','LastLogin','Permission'];
+let Tableheader=['Name','Email','Status','Role','Last login','Permission'];
 
 const tableForUser=document.createElement('table')
 tableForUser.className='table-for-user';
@@ -84,6 +93,7 @@ var t=document.getElementById('table-for-user').appendChild(tableForUser);
 
 list.forEach(user =>{
     let row=document.createElement('tr');
+    row.className='row';
     let count=0;
      Object.values(user).forEach( text=>{
          let rowdata=document.createElement('td');
@@ -95,10 +105,25 @@ list.forEach(user =>{
              iconForUser.src='img/user.png';
              rowdata.appendChild(iconForUser);
          }
+         console.log(text);
          if(count===2){
+             textInSideTD.textContent="";
             rowdata.className='status-column';
+            let imgdata=document.createElement('img')
+            if(text==='Active'){
+                imgdata.src='img/brightActive.png'
+                imgdata.className='status-image';
+                rowdata.appendChild(imgdata);
+            //    rowdata.style.webkitBoxShadow='3px solid rgb(0, 255, 255)';         
+            }else{
+                imgdata.src='img/editedInactive.png'
+                imgdata.className='status-image';
+                rowdata.appendChild(imgdata);
+                // rowdata.style.border='3px solid rgb(200, 151, 151) ';
+                // rowdata.style.color='gray';
+            }
          }
-
+         console.log(user+'PPrinting');
          count++;
          rowdata.appendChild(textInSideTD);
          row.appendChild(rowdata)
