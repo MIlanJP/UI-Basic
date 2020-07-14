bee9989d0956959e043d62f06b89cc15374d2192
kyoolbaba <kyoolbaba@gmail.com>
2020-07-14 16:55:37 +0530

    Made  side nav bar position  fixed

--- app2.js
+++ app2.js
@@ -1,9 +1,11 @@
 let authDropdown=document.querySelector('.vertical-nav-auth-dropdown');
 
-authDropdown.addEventListener('click', function(){
+let authclick=authDropdown.addEventListener('click', function(){
    if(document.getElementById('vertical-nav-dropdown-auth-block')===null){
         document.querySelector('.auth-drop-down').id='vertical-nav-dropdown-auth-block';
+        document.querySelector('.auth-drop-down').style.backgroundColor='#363A43';
    }else{
     document.querySelector('.auth-drop-down').id='vertical-nav-dropdown-auth-display';
-   }
+    document.querySelector('.auth-drop-down').style.backgroundColor='rgb(161, 160, 161)';
+}
 })
